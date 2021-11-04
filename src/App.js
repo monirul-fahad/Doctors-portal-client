8320b07968b9b4f787f2d4e30db04573c50f4462
Monirul Fahad <immonirulfahad@gmail.com>
2021-11-04 18:56:53 +0600

    calender added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./Pages/Home/Home/Home";
+import Appointment from "./Pages/Appointment/Appointment/Appointment";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route path="/about">
-            <Home />
+          <Route path="/appointment">
+            <Appointment></Appointment>
           </Route>
           <Route path="/home">
             <Home />
--- src/Pages/Shared/Navigation/Navigation.js
+++ src/Pages/Shared/Navigation/Navigation.js
@@ -6,6 +6,7 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
+import { Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
@@ -24,7 +25,10 @@ const Navigation = () => {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Doctors Portal
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Link to="/appointment">
+            <Button color="inherit">Appointment</Button>
+          </Link>
+          <Button color="inherit">Log in</Button>
         </Toolbar>
       </AppBar>
     </Box>
